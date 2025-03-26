c1ea78e99e33b8c9c2987c47631c30c5a053e377
MOHAMAD Saifeddin <147329307+msaifeddin@users.noreply.github.com>
2025-03-26 07:43:05 +0100

    Add files via upload

---
+++ fichier/Veille technologique copie/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".sidebar ul li a:hover");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+        
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === current) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
